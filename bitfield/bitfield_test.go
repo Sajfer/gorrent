9df40bf3408e8fd78e1e0ac8d3a32ed4290ceb3f
Anton Bretting <sajfer@gmail.com>
2020-11-22 12:01:06 +0100

    Add tests for bitfield

---
+++ bitfield/bitfield_test.go
@@ -0,0 +1,50 @@
+package bitfield_test
+
+import (
+	"testing"
+
+	"github.com/sajfer/gorrent/bitfield"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHasPiece(t *testing.T) {
+	bf := bitfield.Bitfield{0b01010100, 0b01010100}
+	outputs := []bool{false, true, false, true, false, true, false, false, false, true, false, true, false, true, false, false, false, false, false, false}
+	for i := 0; i < len(outputs); i++ {
+		assert.Equal(t, outputs[i], bf.HasPiece(i))
+	}
+}
+
+func TestSetPiece(t *testing.T) {
+	tests := []struct {
+		input  bitfield.Bitfield
+		index  int
+		output bitfield.Bitfield
+	}{
+		{
+			input:  bitfield.Bitfield{0b01010100, 0b01010100},
+			index:  4,
+			output: bitfield.Bitfield{0b01011100, 0b01010100},
+		},
+		{
+			input:  bitfield.Bitfield{0b01010100, 0b01010100},
+			index:  9,
+			output: bitfield.Bitfield{0b01010100, 0b01010100},
+		},
+		{
+			input:  bitfield.Bitfield{0b01010100, 0b01010100},
+			index:  15,
+			output: bitfield.Bitfield{0b01010100, 0b01010101},
+		},
+		{
+			input:  bitfield.Bitfield{0b01010100, 0b01010100},
+			index:  19, //                            v (noop)
+			output: bitfield.Bitfield{0b01010100, 0b01010100},
+		},
+	}
+	for _, test := range tests {
+		bf := test.input
+		bf.SetPiece(test.index)
+		assert.Equal(t, test.output, bf)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/sajfer/gorrent
 
 go 1.15
 
-require github.com/jackpal/bencode-go v1.0.0
+require (
+	github.com/google/go-cmp v0.5.3 // indirect
+	github.com/jackpal/bencode-go v1.0.0
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.6.1
+	gotest.tools v2.2.0+incompatible
+)
